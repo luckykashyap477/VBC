a4d7a3801cec3d70dbad2c0872b1a69d5289abff
luckykashyap477 <luckykashyap477337@gmail.com>
2025-09-29 22:52:17 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,4 +1,5 @@
 <?php
 session_start();
 $con=mysqli_connect("bwlslb1rt9c08mofpx80-mysql.services.clever-cloud.com","uyvpwc55sluohcde","z7S7QGcaukT9ieKeF3E9","bwlslb1rt9c08mofpx80");
+echo "connected to db "+$con;
 ?>
