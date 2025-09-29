939da3378cd3558ad8dc44fe02b71e7fd9a738ad
luckykashyap477 <luckykashyap477337@gmail.com>
2025-09-29 22:47:37 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,4 +1,4 @@
 <?php
 session_start();
-$con=mysqli_connect("localhost","root","","vbc_live");
+$con=mysqli_connect("bwlslb1rt9c08mofpx80-mysql.services.clever-cloud.com","uyvpwc55sluohcde","z7S7QGcaukT9ieKeF3E9","bwlslb1rt9c08mofpx80");
 ?>
